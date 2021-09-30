1ec3c5bf4d6bb1470a816e57edb086ad92abf78f
Thomas HAMON <thomas.hamon@irisa.fr>
2021-09-30 09:15:02 +0200

    cors added to the project

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const bodyParser = require("body-parser");
 const routesArticle = require('./routes/article')
 const routesLogin = require('./routes/login')
 
-
+let corsOption = {
+  origin : '*'
+}
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/articles', routesArticle)
 app.use('/login', routesLogin)
-app.use(cors())
+app.use(cors(corsOption))
 
 
 
